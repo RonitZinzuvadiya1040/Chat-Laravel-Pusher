063314393f55b0a0a93972318ff59e57486de8fe
RonitZinzuvadiya1040 <ronitzinzuvadiya2001@gmail.com>
2024-07-15 15:50:49 +0530

    Laravel Real Time Chat App

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,4 @@
+<div class="right message">
+    <p>{{ $message }}</p>
+    <img src="{{ url('/images/myimage.jpg') }}" alt="Avatar">
+</div>
---
+++ resources/views/receive.blade.php
@@ -0,0 +1,4 @@
+<div class="left message">
+    <img src="{{ url('/images/myimage.jpg') }}" alt="Avatar">
+    <p>{{ $message }}</p>
+</div>
